824bdb2314ee4d01fc4b963e77f797593510e1d3
ma310u2 <47171129+ma310u2@users.noreply.github.com>
2019-04-13 17:59:49 +0900

    Update jquery.wavify.js

--- jquery.wavify.js
+++ jquery.wavify.js
@@ -7,12 +7,5 @@ var myWave = $('#wave-1').wavify({
   color: 'rgb(0,187,221,0.5)',
   speed: .25
 });
-  var myWave = $('#wave-2').wavify({
-  height: 60,
-  bones: 3,
-  amplitude: 40,
-  color: 'rgb(255,255,255,0.5)',
-  speed: .25
-});
 
 });
