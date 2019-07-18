30ad89faab14a8bc03947f8cade87834edd5f590
ma310 <pta3pta3pta3@gmail.com>
2019-07-18 23:18:52 +0900

    整列

--- jquery.wavify.js
+++ jquery.wavify.js
@@ -1,11 +1,11 @@
 $(function () {
 
-var myWave = $('#wave-1').wavify({
-  height: 60,
-  bones: 3,
-  amplitude: 40,
-  color: 'rgb(0,187,221,0.5)',
-  speed: .25
-});
+    var myWave = $('#wave-1').wavify({
+        height: 60,
+        bones: 3,
+        amplitude: 40,
+        color: 'rgb(0,187,221,0.5)',
+        speed: .25
+    });
 
 });
